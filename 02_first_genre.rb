eaf7eece7bf5fbfd4b6447ab0cd2645f4494d55f
Finnegan Hewitt <fbhewitt@gmail.com>
2015-02-11 09:22:50 -0700

    Finish 02

--- 02_first_genre.rb
+++ 02_first_genre.rb
@@ -1,3 +1,6 @@
 require_relative 'people'
 
+puts PEOPLE["joe"][:preferences][:favorite_genres].first
+puts PEOPLE["sue"][:preferences][:favorite_genres].first
+
 # What is the first genre that each person listed?
